fc6037a06a0116cff75886f12508dfa97f26a36b
Cesarlara998 <50812956+Cesarlara998@users.noreply.github.com>
2019-09-01 18:04:33 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import socket
 def main():
     socket_server = socket.socket()
-    socket_server.bind( ('localhost',8888) )
+    socket_server.bind( ('localhost',80) )
     socket_server.listen(5)
     while True:
         conexion, addr = socket_server.accept()
